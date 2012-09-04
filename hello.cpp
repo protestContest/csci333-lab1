841cf21161adc117e971642fc257f20c591bb96f
Zack Michener <zack@zackmichener.com>
2012-09-04 13:34:32 -0400

    solved euler problem 1

--- hello.cpp
+++ hello.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
-
 using std::cout;
 using std::endl;
 
-int main() {
-	cout << "Hello, world!" << endl;
+int main(int argc, char** argv) {
+
+	int sum = 0;
+	for (int i = 0; i < 1000; i++) {
+		if (i%3 == 0 || i%5 == 0) {
+			sum += i;
+		}
+	}
+
+	cout << sum << endl;
 
 	return 0;
 }
